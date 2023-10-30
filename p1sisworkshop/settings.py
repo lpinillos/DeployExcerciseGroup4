f029b01884ce4ec01500335cb6641ee1f5999784
Luis Pinillos <pinillosluisfer@gmail.com>
2023-10-30 10:41:31 -0500

    Minor fixes in settings

--- p1sisworkshop/settings.py
+++ p1sisworkshop/settings.py
@@ -13,13 +13,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 DEBUG = False
+BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = ['*']
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
